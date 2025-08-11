eebe79c5de9c9c4fcd90134a18089b04aad7adbe
shinejohn <john_shine@hotmail.com>
2025-08-10 22:04:41 -0400

    Fix TaskMarketplace prop error - Remove unused userId prop

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -13,7 +13,7 @@ export default async function MarketplacePage() {
   
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <TaskMarketplace userId={user.id} />
+      <TaskMarketplace />
     </Suspense>
   )
 }
